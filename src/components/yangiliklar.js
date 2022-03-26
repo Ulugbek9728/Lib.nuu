5af3b24039b7547e55862f145be4002b71466f18
ulugbek9728 <ulugbekshukurov9728@gmail.com>
2022-03-26 11:22:18 +0500

    yangiliklar page tugadi (desktop)

--- src/components/yangiliklar.js
+++ src/components/yangiliklar.js
@@ -1,12 +1,193 @@
 import React from 'react';
 import {Link} from "react-router-dom";
+import OwlCarousel from 'react-owl-carousel';
+import 'owl.carousel/dist/assets/owl.carousel.min.css'
+import 'owl.carousel/dist/assets/owl.theme.default.css'
+
+import './Yangiliklar.scss'
 import Footer from "./footer";
 
 function Yangiliklar(props) {
     return (
-        <div>
-            <Link to={'/'}>Home page</Link>
-            <div>yangiliklar</div>
+        <div className='YangiliklarCompon'>
+            <div className='kitoblar'>
+                <div className="line">
+                    <Link className='asosiyLink' to={'/'}>Asosiy sahifa / Yangiliklar</Link>
+                    <div className="libLogo">
+                        <img src="../img/logo_lib.svg" alt=""/>
+                    </div>
+                    <div className="text">
+                        Mirzo ulug’bek nomidagi O’zbekiston milliy universiteti kutubxonosining rasmiy veb sayti
+                    </div>
+                </div>
+            </div>
+            <div className="title">Yangiliklar</div>
+            <hr/>
+            <div className="boxYangiliklar">
+                <img src="./img/qo'l.png" alt=""/>
+                <div className="right">
+                    <h1>The Most Popular Celebrity Name List of
+                        the Millennium is
+                        Finally Here
+                    </h1>
+                    <p>I undorstand how that could positivaly effect your body. but
+                        alchamizing means turming elements
+                        Laura sebastian January 7, 2022
+                    </p>
+                    <button className='btn'>Batafsil</button>
+                </div>
+            </div>
+
+            <div className="container-fluid">
+                <div className="row smolNewBox">
+                    <div className="col-3 smolNew">
+                        <img src="./img/women.png" alt=""/>
+                        <p>
+                            Krestian Stewn Was Seen ILaving Lunch in toronto with Boyfriend
+                        </p>
+                        <hr/>
+                        <div className="boxcha">
+                            <div className="left">
+                                <img src="./img/Group5.png" alt=""/>
+                                <span>101 Ko’rilgan</span>
+                            </div>
+                            <button className='btn'>Batafsil</button>
+                        </div>
+                    </div>
+                    <div className="col-3 smolNew">
+                        <img src="./img/men.png" alt=""/>
+                        <p>
+                            Krestian Stewn Was Seen ILaving Lunch in toronto with Boyfriend
+                        </p>
+                        <hr/>
+                        <div className="boxcha">
+                            <div className="left">
+                                <img src="./img/Group5.png" alt=""/>
+                                <span>101 Ko’rilgan</span>
+                            </div>
+                            <button className='btn'>Batafsil</button>
+                        </div>
+                    </div>
+                    <div className="col-3 smolNew">
+                        <img src="./img/women.png" alt=""/>
+                        <p>
+                            Krestian Stewn Was Seen ILaving Lunch in toronto with Boyfriend
+                        </p>
+                        <hr/>
+                        <div className="boxcha">
+                            <div className="left">
+                                <img src="./img/Group5.png" alt=""/>
+                                <span>101 Ko’rilgan</span>
+                            </div>
+                            <button className='btn'>Batafsil</button>
+                        </div>
+                    </div>
+                    <div className="col-3 smolNew">
+                        <img src="./img/men.png" alt=""/>
+                        <p>
+                            Krestian Stewn Was Seen ILaving Lunch in toronto with Boyfriend
+                        </p>
+                        <hr/>
+                        <div className="boxcha">
+                            <div className="left">
+                                <img src="./img/Group5.png" alt=""/>
+                                <span>101 Ko’rilgan</span>
+                            </div>
+                            <button className='btn'>Batafsil</button>
+                        </div>
+                    </div>
+
+                    <div className="col-3 smolNew">
+                        <img src="./img/women.png" alt=""/>
+                        <p>
+                            Krestian Stewn Was Seen ILaving Lunch in toronto with Boyfriend
+                        </p>
+                        <hr/>
+                        <div className="boxcha">
+                            <div className="left">
+                                <img src="./img/Group5.png" alt=""/>
+                                <span>101 Ko’rilgan</span>
+                            </div>
+                            <button className='btn'>Batafsil</button>
+                        </div>
+                    </div>
+                    <div className="col-3 smolNew">
+                        <img src="./img/men.png" alt=""/>
+                        <p>
+                            Krestian Stewn Was Seen ILaving Lunch in toronto with Boyfriend
+                        </p>
+                        <hr/>
+                        <div className="boxcha">
+                            <div className="left">
+                                <img src="./img/Group5.png" alt=""/>
+                                <span>101 Ko’rilgan</span>
+                            </div>
+                            <button className='btn'>Batafsil</button>
+                        </div>
+                    </div>
+                    <div className="col-3 smolNew">
+                        <img src="./img/women.png" alt=""/>
+                        <p>
+                            Krestian Stewn Was Seen ILaving Lunch in toronto with Boyfriend
+                        </p>
+                        <hr/>
+                        <div className="boxcha">
+                            <div className="left">
+                                <img src="./img/Group5.png" alt=""/>
+                                <span>101 Ko’rilgan</span>
+                            </div>
+                            <button className='btn'>Batafsil</button>
+                        </div>
+                    </div>
+                    <div className="col-3 smolNew">
+                        <img src="./img/men.png" alt=""/>
+                        <p>
+                            Krestian Stewn Was Seen ILaving Lunch in toronto with Boyfriend
+                        </p>
+                        <hr/>
+                        <div className="boxcha">
+                            <div className="left">
+                                <img src="./img/Group5.png" alt=""/>
+                                <span>101 Ko’rilgan</span>
+                            </div>
+                            <button className='btn'>Batafsil</button>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+            <div className="galereya">Foto Galereya</div>
+
+            <OwlCarousel
+                className="owl-theme"
+                items="3"
+                autoplay={true}
+                autoplayTimeout="2000"
+                nav={false}
+                loop={true}
+                dots={false}
+            >
+                <div className="item">
+                    <img src="./img/galerey1.png" alt=""/>
+                </div>
+                <div className="item">
+                    <img src="./img/galerey2.png" alt=""/>
+                </div>
+                <div className="item">
+                    <img src="./img/galerey3.png" alt=""/>
+                </div>
+                <div className="item">
+                    <img src="./img/galerey4.png" alt=""/>
+                </div>
+                <div className="item">
+                    <img src="./img/galerey5.png" alt=""/>
+                </div>
+                <div className="item">
+                    <img src="./img/galerey6.png" alt=""/>
+                </div>
+
+            </OwlCarousel>
             <Footer/>
         </div>
     );
